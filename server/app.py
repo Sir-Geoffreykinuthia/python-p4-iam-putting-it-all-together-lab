58929ece9d874c4d9e9ca3c2d986d1123a81a1e5
geoffrey kinuthia <geoffreykinuthia10@gmail.com>
2023-07-09 11:33:05 +0300

    Commitiing

--- server/app.py
+++ server/app.py
@@ -4,18 +4,22 @@ from flask import request, session
 from sqlalchemy.exc import IntegrityError
 from flask_restful import Resource
 
-
 from config import app, db, api
 from models import User, Recipe
 
 class Signup(Resource):
     def post(self):
-        json_infor = request.get_json()
+        json_info = request.get_json()
+        username = json_info.get('username')
+        password = json_info.get('password')
+        image_url = json_info.get('image_url')
+        bio = json_info.get('bio')
+        
         user = User(
-            username=json_infor.get['username'],
-            password_hash=json_infor.get['password'],
-            image_url=json_infor.get('image_url'),
-            bio=json_infor.get('bio')
+            username=username,
+            password=password,
+            image_url=image_url,
+            bio=bio
         )
         try:
             db.session.add(user)
@@ -35,14 +39,16 @@ class CheckSession(Resource):
             if user:
                 return user.to_dict(), 200
         return {'error': 'Unauthorized.'}, 401
-
     
 
 class Login(Resource):
     def post(self):
         json = request.get_json()
-        user = User.query.filter_by(username=json['username']).first()
-        if user and user.authenticate(json['password']):
+        username = json.get('username')
+        password = json.get('password')
+        
+        user = User.query.filter_by(username=username).first()
+        if user and user.authenticate(password):
             session['user_id'] = user.id
             return user.to_dict(), 200
         else:
@@ -61,7 +67,8 @@ class Logout(Resource):
 class RecipeIndex(Resource):
     def get(self):
         if 'user_id' in session:
-            recipes = Recipe.query.all()
+            user_id = session['user_id']
+            recipes = Recipe.query.filter_by(user_id=user_id).all()
             return [recipe.to_dict() for recipe in recipes], 200
         else:
             return {'error': 'Unauthorized.'}, 401
@@ -69,10 +76,14 @@ class RecipeIndex(Resource):
     def post(self):
         if 'user_id' in session:
             json = request.get_json()
+            title = json.get('title')
+            instructions = json.get('instructions')
+            minutes_to_complete = json.get('minutes_to_complete')
+            
             recipe = Recipe(
-                title=json['title'],
-                instructions=json['instructions'],
-                minutes_to_complete=json['minutes_to_complete'],
+                title=title,
+                instructions=instructions,
+                minutes_to_complete=minutes_to_complete,
                 user_id=session['user_id']
             )
             db.session.add(recipe)
@@ -80,8 +91,6 @@ class RecipeIndex(Resource):
             return recipe.to_dict(), 201
         else:
             return {'error': 'Unauthorized.'}, 401
-
-    
 
 api.add_resource(Signup, '/signup', endpoint='signup')
 api.add_resource(CheckSession, '/check_session', endpoint='check_session')
